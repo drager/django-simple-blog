39519bde6120f5a06c59c17774ee64e34d0107ca
Emad Mokhtar <emad.m.habib@gmail.com>
2016-10-03 15:55:28 +0300

    Remove patterns it's not support by Django 1.10

--- simpleblog/urls.py
+++ simpleblog/urls.py
@@ -1,16 +1,17 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from .views import BlogListView, BlogDetailView, LatestEntriesFeed
 
-urlpatterns = patterns('simpleblog.views',
-                       url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-_\w]+)/$',
-                           BlogDetailView.as_view(),
-                           name='blog_detail',
-                           ),
-                       url(r'^archive/$',
-                           BlogListView.as_view(
-                               template_name="simpleblog/post_archive.html",
-                               page_template="simpleblog/post_archive_page.html"),
-                           name="blog_archive"),
-                       url(r'^latest/feed/$', LatestEntriesFeed()),
-                       )
+urlpatterns = [
+                url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-_\w]+)/$',
+                    BlogDetailView.as_view(),
+                    name='blog_detail',
+                    ),
+                url(r'^archive/$',
+                    BlogListView.as_view(
+                        template_name="simpleblog/post_archive_page.html",
+                        page_template="simpleblog/post_archive_page.html"),
+                        name="blog_archive"),
+                url(r'^latest/feed/$', LatestEntriesFeed()),
+                url(r'^$', BlogListView.as_view(), name='blog_index'),
+            ]
