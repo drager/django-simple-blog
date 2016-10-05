12cb0238e611f97ccd44d70d9d3fdc6f353d4eb5
Emad Mokhtar <emad.m.habib@gmail.com>
2016-10-05 11:51:43 +0300

    Add Python3

--- setup.py
+++ setup.py
@@ -36,6 +36,8 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
