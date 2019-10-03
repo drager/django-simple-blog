10ce633d49f99fec51b2274510bc648eab5a9111
Nikos Mavrakis <mavrakis.n@gmail.com>
2019-10-03 11:15:28 +0300

    Fix backwards compatibility issue #24

--- simpleblog/urls.py
+++ simpleblog/urls.py
@@ -1,17 +1,24 @@
-from django.urls import re_path
+try:
+    from django.urls import re_path as u
+except ModuleNotFoundError as e:
+    from django.conf.urls import url as u
+
 
 from .views import BlogDetailView, BlogListView, LatestEntriesFeed
 
+
 urlpatterns = [
-                re_path(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-_\w]+)/$',
-                    BlogDetailView.as_view(),
-                    name='blog_detail',
-                    ),
-                re_path(r'^archive/$',
-                    BlogListView.as_view(
-                        template_name="simpleblog/post_archive.html",
-                        page_template="simpleblog/post_archive_page.html"),
-                        name="blog_archive"),
-                re_path(r'^latest/feed/$', LatestEntriesFeed()),
-                re_path(r'^$', BlogListView.as_view(), name='blog_index'),
-            ]
+    u(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-_\w]+)/$',
+      BlogDetailView.as_view(),
+      name='blog_detail'
+    ),
+    u(r'^archive/$',
+      BlogListView.as_view(
+      template_name="simpleblog/post_archive.html",
+      page_template="simpleblog/post_archive_page.html"),
+      name="blog_archive"
+    ),
+    u(r'^latest/feed/$', LatestEntriesFeed()),
+    u(r'^$', BlogListView.as_view(), name='blog_index')
+]
+
